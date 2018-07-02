b430eeb9beb43f368e8fd23d72f476af298d6ed8
au12113 <wasupont@windowslive.com>
2018-07-02 15:42:36 +0700

    +add task

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var fs = require("fs")
+var fs = require('fs')
+// var bodyParser = require('body-parser')
+var tasks = require('./tasks.json')
 
-var tasks = require("./tasks.json")
+// app.use(bodyParser)
 
 //
-app.get("/", function(req, res) {
-  // res.sendStatus(200)
-  res.send("HELLO");
-  // console.log("Hello")
+app.get('/', function (req, res) {
+  res.status(200).send('HELLO');
+  // console.log('Hello')
 });
 
 // view all items in the list
-app.get("/task/", function(req, res) {
+app.get('/all-tasks/', function (req, res) {
+  res.status(200).send(JSON.stringify(tasks))
+});
+
+// view specific item in list
+app.get('/tasks/:id', function (req, res) {
   var task_id = req.param('id');
-  for(var i = 0; i < task.length; i++) {
-    if(task[i].id == task_id) {
-      res.send("task_id: " + task[i].id + "\nsubject: " + task[i].subject + "\n ");
+  for (var i = 0; i < tasks.length; i++) {
+    console.log('i = ' + i)
+    if (tasks[i].id == task_id) {
+      return res.status(200).send(tasks[i]);
     }
   }
+  return res.sendStatus(404)
 });
 
 // add item to list
-app.post("/add", function(req, res) {
+app.post('/add', function (req, res) {
   var task = {
-    id : req.param('id'),
-    subject : req.param('subject'),
-    description : req.param('description'),
-    done : false
+    'id': req.param('id'),
+    'subject': req.param('subject'),
+    'description': req.param('description'),
+    'done': false
+  }
+  // console.log(task)
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == task.id) {
+      return res.status(409)
+    }
   }
   tasks.push(task)
-  fs.writeFile("./tasks.json", tasks)
+  fs.writeFile('./tasks.json', JSON.stringify(tasks), function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(404)
+    }
+    res.status(201).send('added task.')
+    console.log("The file was saved!");
+  })
 })
 
 app.listen(3000);
