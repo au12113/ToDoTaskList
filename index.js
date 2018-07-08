108e8aadb0215244064f33a4a26325b1f7502bc5
au12113 <wasupont@windowslive.com>
2018-07-09 03:25:39 +0700

    add edit task, update status and delete
    - add edit task detail, update status and delete object
    - change get all tasks url
    - post task with query object instead
    
    Next
    - 405 handler
    - add detail in edit detail

--- index.js
+++ index.js
@@ -12,33 +12,107 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://ds127771.mlab.com:2777
 app.use(bodyParser.json())
 
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    user: 'someone',
-    pass: '6characters'
+  useNewUrlParser: true,
+  user: 'someone',
+  pass: '6characters'
 })
 
-app.get('/', function(req, res) {
-    Tasks.find().exec(function(err, objects) {
-        if(err) {
-            return res.sendStatus(500)
-        }
-        res.jsonp(objects)
-    })
+// get all tasks
+app.get('/tasks', function (req, res) {
+  Tasks.find().exec(function (err, tasks) {
+    if (err) {
+      return res.sendStatus(500)
+    }
+    return res.status(200).jsonp(tasks)
+  })
 })
 
-app.post('/add', function(req, res) {
-    var newTask = new Tasks({
-        subject : req.params.subject,
-        description: req.params.description,
-        status: false
-    })
-    newTask.save(function(err, task) {
-        if(err) { 
-            return res.sendStatus(500)
-        }
-        console.log(task._id)
-        return res.sendStatus(201)
-    })
+// get specific task by id
+app.get('/tasks/:id', function (req, res) {
+  Tasks.findOne({
+    _id: req.params.id
+  }).exec(function (err, task) {
+    if (err) {
+      return res.sendStatus(500)
+    }
+    return res.status(200).jsonp(task)
+  })
+})
+
+// add new task with false status
+app.post('/tasks', function (req, res) {
+  var newTask = new Tasks({
+    subject: req.query.subject,
+    description: req.query.description,
+    status: false
+  })
+  newTask.save(function (err, task) {
+    if (err) {
+      return res.sendStatus(500)
+    }
+    console.log(task._id)
+    return res.sendStatus(201)
+  })
+})
+
+// edit task detail
+app.patch('/tasks/:id', function (req, res) {
+  Tasks.findByIdAndUpdate(req.params.id, {
+    $set: {},
+    $inc: {
+      __v: 1
+    }
+  }, function (err, task) {
+    if (err) {
+      if (err.reason == undefined) {
+        return res.sendStatus(404)
+      } else {
+        return res.sendStatus(500)
+      }
+    }
+    return res.status(200).jsonp(task)
+  })
+
+})
+
+// set/update task status
+app.patch('/tasks/:id/status', function (req, res) {
+  Tasks.findByIdAndUpdate(req.params.id, {
+    $set: {
+      status: true
+    },
+    $inc: {
+      __v: 1
+    }
+  }, function (err, task) {
+    if (err) {
+      if (err.reason == undefined) {
+        return res.sendStatus(404)
+      } else {
+        return res.sendStatus(500)
+      }
+    }
+    return res.status(200).jsonp(task)
+  })
+})
+
+// delete task by id
+app.delete('/tasks/:id/', function (req, res) {
+  Tasks.findOneAndRemove({
+    _id: req.params.id
+  }).exec(function (err, task) {
+    if (err) {
+      if (err.reason == undefined) {
+        return res.sendStatus(404)
+      } else {
+        return res.sendStatus(500)
+      }
+    }
+    if (!task) {
+      return res.sendStatus(404)
+    }
+    res.sendStatus(200)
+  })
 })
 
 app.listen(PORT)
